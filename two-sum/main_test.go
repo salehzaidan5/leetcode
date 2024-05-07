ea80fbf36d12d995f9611c9f3fa591bd32782b09
salehzaidan <salehzaidan24@gmail.com>
2024-05-07 23:44:33 +0700

    Solve two-sum in O(n^2) time

---
+++ two-sum/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExample(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		want   []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%v,%d", tt.nums, tt.target)
+		t.Run(testname, func(t *testing.T) {
+			ans := twoSum(tt.nums, tt.target)
+			assert.Equal(t, tt.want, ans)
+		})
+	}
+}
