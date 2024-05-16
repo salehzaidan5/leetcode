2a088424cc65234b0540e46e8fd77720735cc895
salehzaidan <salehzaidan24@gmail.com>
2024-05-16 19:57:52 +0700

    Refactor plus-one

--- plus-one/main.go
+++ plus-one/main.go
@@ -3,35 +3,15 @@ package main
 import "slices"
 
 func plusOne(digits []int) []int {
-	carry := false
-	done := false
-	for i := len(digits) - 1; i >= 0 && !done; i-- {
-		if carry {
-			if digits[i] == 9 {
-				digits[i] = 0
-				continue
-			}
-
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] != 9 {
 			digits[i] += 1
-			done = true
-			carry = false
-			continue
+			return digits
 		}
 
-		if digits[i] == 9 {
-			digits[i] = 0
-			carry = true
-			continue
-		}
-
-		digits[i] += 1
-		done = true
-		break
+		digits[i] = 0
 	}
 
-	if carry {
-		digits = slices.Insert(digits, 0, 1)
-	}
-
+	digits = slices.Insert(digits, 0, 1)
 	return digits
 }
